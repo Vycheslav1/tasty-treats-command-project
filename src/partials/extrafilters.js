923bb63f974a36f3e6401b1fe13428d8e7e50d47
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-07-23 15:24:36 +0300

    Revert "відредаговано стилі"
    This reverts commit 4782cff75dabd0de369a98e20f695ead21367462.

--- src/partials/extrafilters.js
+++ src/partials/extrafilters.js
@@ -1,19 +1,2 @@
 // навісити на extra-filters-form-select новий клас, коли опція обрана option-choosed
 // навісити клас , коли починаємо писати в інпуті Сьорч формт extra-filters-search-icon-close
-import axios from 'axios';
-
-const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
-
-const refs = {
-  formExtraFilters: document.querySelector('.extra-filters-form'),
-  inputExtraFiltersForm: document.querySelector('.extra-filters-form-input'),
-  searchResetBtnInputExtraFiltersForm: document.querySelector(
-    '[data-search-reset]'
-  ),
-  timeExtraFiltersForm: document.querySelector('#time'),
-  areaExtraFiltersForm: document.querySelector('#area'),
-  ingredientsExtraFiltersForm: document.querySelector('#ingredients'),
-  resetBtnTotalExtraFiltersForm: document.querySelector(
-    '.extra-filters-reset-btn'
-  ),
-};
