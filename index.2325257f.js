82041bd6ee89ca394633283f252a391c8c936bc4
Vycheslav1 <41229160+Vycheslav1@users.noreply.github.com>
2023-07-25 18:29:34 +0000

    Deploying to gh-pages from @ Vycheslav1/tasty-treats-command-project@a91e02e77e7a280014056fbe899e0cb6dcb87d70 🚀

--- index.2325257f.js
+++ index.2325257f.js
@@ -1,2 +1,2 @@
 !function(){var e=document.querySelector(".header-menu-container"),t=document.querySelector(".menu-toggle"),n=document.querySelector(".burger-switch-input");t.addEventListener("click",(function(){e.classList.toggle("active")})),n.addEventListener("change",(function(e){e.target.checked?console.log("Переключатель включен"):console.log("Переключатель выключен")}))}();
-//# sourceMappingURL=favorites-page.97b2e875.js.map
+//# sourceMappingURL=index.2325257f.js.map
