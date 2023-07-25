25bf306d8307b8529a34d9e307f9613cb0bab1e5
Vycheslav1 <41229160+Vycheslav1@users.noreply.github.com>
2023-07-25 15:31:27 +0000

    Deploying to gh-pages from @ Vycheslav1/tasty-treats-command-project@aa5235a086dd2f3a6eb1a8240b3ead36837d0678 🚀

---
+++ favorites-page.eb68bd11.js
@@ -0,0 +1,2 @@
+function scrollToTop(){window.scrollTo({top:0,behavior:"smooth"})}var scrollToTopButton=document.getElementById("scrollToTopButton");scrollToTopButton.style.display="none",window.addEventListener("scroll",(function(){window.scrollY>=50?scrollToTopButton.style.display="block":scrollToTopButton.style.display="none"})),scrollToTopButton.addEventListener("click",scrollToTop);
+//# sourceMappingURL=favorites-page.eb68bd11.js.map
