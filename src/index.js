419f6d62f9b6b535b8772e648c543a3394733f26
Vycheslav1 <41229160+Vycheslav1@users.noreply.github.com>
2023-07-27 16:00:20 +0300

    project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './js/favorites';
+/*import './js/favorites';
 import './js/recipe';
 import './js/switcher';
 import './js/scroll-up';
@@ -6,4 +6,25 @@ import './js/slider';
 import './js/header-burger';
 import './js/home_categories-api';
 import './js/create_order.js';
-import './js/extrafilters';
+import './js/extrafilters';*/
+
+// import './js/recipe';
+import './js/switcher';
+// import './js/scroll-up';
+import './js/slider';
+import './js/header-burger';
+// import './js/extrafilters';
+import './js/theme-switcher';
+// import './js/home_categories-api';
+import './js/create_order.js';
+import './js/popular-recipes';
+
+
+
+
+
+
+
+
+
+
---
+++ src/js/create_order.js
@@ -0,0 +1,29 @@
+
+const openButtonHeroModal = document.querySelector('.button-order-now-hero-js');
+const closeButtonModal = document.querySelector(
+  '.order-now-modal-button-close-js'
+);
+const backdrop = document.querySelector('.js-backdrop');
+// const formModalOrderNow = document.querySelector('.order-now-modal-form');
+openButtonHeroModal.addEventListener('click', onClickModalOpen);
+closeButtonModal.addEventListener('click', onClickModalRemove);
+backdrop.addEventListener('click', onClickBackdrop);
+// formModalOrderNow.addEventListener('submit', onSubmitForm);
+function onClickModalOpen() {
+  window.addEventListener('keydown', onEscKeyPress);
+  document.body.classList.add('show-modal-order-now');
+}
+function onClickModalRemove() {
+  window.removeEventListener('keydown', onEscKeyPress);
+  document.body.classList.remove('show-modal-order-now');
+}
+function onClickBackdrop(event) {
+  if (event.currentTarget === event.target) {
+    onClickModalRemove();
+  }
+}
+function onEscKeyPress(event) {
+  if (event.code === 'Escape') {
+    onClickModalRemove();
+  }
+}
