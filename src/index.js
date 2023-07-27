d8d924f17a84e6596c5fadcce410175396474c13
rasinoleg <rasinoleg3@gmail.com>
2023-07-27 11:26:34 +0300

    testy favorites

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './js/favorites';
 import './js/recipe';
 import './js/switcher';
 import './js/scroll-up';
-import './js/slider';
+// import './js/slider';
 import './js/header-burger';
-import './js/home_categories-api';
+// import './js/home_categories-api';
 import './js/create_order.js';
-import './js/extrafilters';
+// import './js/extrafilters';
+import './js/favorites';
