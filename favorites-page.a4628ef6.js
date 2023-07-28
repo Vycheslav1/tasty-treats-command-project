f54e1109c6f2d7ea566c7ce66a4b3e31d8d893fd
TanyaMosk <123247337+TanyaMosk@users.noreply.github.com>
2023-07-28 07:22:11 +0000

    Deploying to gh-pages from @ Vycheslav1/tasty-treats-command-project@ea08036973641f0bc06aa613a944dbd7239474d3 🚀

---
+++ favorites-page.a4628ef6.js
@@ -0,0 +1,2 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},o={},r=e.parcelRequire4b17;null==r&&((r=function(e){if(e in n)return n[e].exports;if(e in o){var r=o[e];delete o[e];var i={id:e,exports:{}};return n[e]=i,r.call(i.exports,i,i.exports),i.exports}var l=new Error("Cannot find module '"+e+"'");throw l.code="MODULE_NOT_FOUND",l}).register=function(e,n){o[e]=n},e.parcelRequire4b17=r),r("aUbok"),r("2fMSh");
+//# sourceMappingURL=favorites-page.a4628ef6.js.map
