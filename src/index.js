b69ace90c6aff80f67f69716f05cb156e8df696c
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-07-28 10:48:23 +0300

    criet-order

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
-
-/*import './js/favorites';
-import './js/switcher';
-// import './js/scroll-up';
-import './js/slider';
 import './js/header-burger';
-// import './js/home_categories-api';
-import './js/create_order.js';
-// import './js/extrafilters';*/
-// import './js/recipe';
-// import './js/scroll-up';
+import './js/create-order';
+import './js/switcher';
+import './js/scroll-up';
+import './js/slider';
+import './js/extrafilters';
 import './js/theme-switcher';
-import './js/create_order.js';
+import './js/home_categories-api';
 import './js/popular-recipes';
 import './js/add-rating';
-
-
-
-
-
-
-
-
-
-
-
+import './js/recipe';
