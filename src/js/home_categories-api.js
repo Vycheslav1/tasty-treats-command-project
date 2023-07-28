8401c2854ab30384407e22bb92a16fb4d7a8697b
Vycheslav1 <41229160+Vycheslav1@users.noreply.github.com>
2023-07-28 15:07:43 +0300

    allcategories

--- src/js/home_categories-api.js
+++ src/js/home_categories-api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import Notiflix from 'notiflix';
+
 import Pagination from 'tui-pagination';
 
 import 'tui-pagination/dist/tui-pagination.css';
@@ -33,7 +35,7 @@ let perPage;
 const recipes = document.querySelector(".nav-scroller");
 
 const gallery = document.querySelector(".photos .pictures-gallery");
-const things = document.querySelector(".photos");
+//const things = document.querySelector(".photos");
 const allCategories=document.querySelector(".all-categories");
 
 const plugCover=document.querySelector(".plug");
@@ -58,30 +60,36 @@ sort="";
 //  return results;
 //};
 
+//document.addEventListener("DOMContentLoaded",()=>{
+
+//});
+
 function renderCardsList(foods) {
 //console.log(foods);
 //localStorage.setItem("results",foods);
 
 
   let markup = ``;
- 
+
+  
   for (let i = 0; i < pictures[0].results.length; i += 1) {
-   
+    let stars=`<div class="all-stars">`;
+    for(let j=0;j<5;j+=1)
+    {
+     if(j<Math.floor(pictures[0].results[i].rating))
+      {
+     stars+=`<span class="fa fa-star checked"></span>`
+      }else{
+        stars+=`<span class="fa fa-star star_color"></span>`
+      }
+    }
+    stars+=`</div>`
     markup += `<li><div class="photo-card"><img class="picture"  src=${pictures[0].results[i].preview} alt=${pictures[0].results[i].tags[0]} loading="lazy" />
- <h2 class="card-title">${pictures[0].results[i].title}</h2><p class="recipe-description">${pictures[0].results[i].description}</p>
-  <div class="rating" >
-  <div class="rating_body">
-  <div class="rating_active"></div>
-      <div class="rating_items"></div>
-  </div>  
-  <div class="rating_value" >${pictures[0].results[i].rating}</div>  
-</div>
-<button type="button" class="get-recipes">See recipe</button></div><label class="label-check">
-<input class="modal-check" type="checkbox" id="check-item" />
-<svg class="checkmark">
-  <use href="./css/images/sprite/icons.svg#icon-heart"></use>
-</svg>
-</label></li>`;
+ <h2 class="card-title">${pictures[0].results[i].title}</h2><p class="recipe-description">${pictures[0].results[i].description}</p><p class="recipe_rating"}>${pictures[0].results[i].rating}</p>`+stars+
+ `<button type="button" class="good-recipes">See recipe</button><label id="brand" class="label-check">
+<input class="modal-check " type="checkbox" id="check-item" />
+<i class="fa fa-heart" ></i>
+</label></div></li>`;
   };//};
   
 
@@ -91,12 +99,18 @@ function renderCardsList(foods) {
  //{
  check.addEventListener("click",(evt)=>{
   const modal=document.querySelectorAll(".modal-check");
+
+ const heart=document.querySelectorAll(".photo-card .label-check .fa-heart");
+//console.log(heart);
+ 
  
   for(let i=0;i<pictures[0].results.length;i+=1)
   {
     if(modal[i].checked)
     {
-  localStorage.setItem("favorites",pictures[i]);
+      evt.target.classList.toggle("change-color");
+      console.log(i);
+  localStorage.setItem("favorites",JSON.stringifypictures[i]);
     }else{
       for(let j=0;j<favorites.length;j=+1)
       {
@@ -104,7 +118,7 @@ function renderCardsList(foods) {
         {
           favorites.splice(j,1);
 
-          localStorage.setItem("favorites",favorites);
+          localStorage.setItem("favorites",JSON.stringify(favorites));
         }
       }
       
@@ -112,7 +126,7 @@ function renderCardsList(foods) {
   }
  });
 
- //gallery.addEventListener("load",()=>{
+ //document.addEventListener("change",()=>{
 
  //const ratings=document.querySelectorAll(".rating");
  // console.log("hello");
@@ -214,7 +228,7 @@ axios.get(base_url,{params:{category:sort,page:loadPage,limit:perPage}}).then(re
 //  /<p class="plug-text">Sorry, there are no images matching your search query. Please try again</p></div>`;
 //plugCover.classList.toggle(".is-hidden");
  
-    return;
+Notiflix.Report.warning('Warning', "Sorry, there are no images matching your search query. Please try again", 'Warning');
   } else {
 
     pictures.push(response.data);
@@ -238,14 +252,19 @@ axios.get(base_url,{params:{category:sort,page:loadPage,limit:perPage}}).then(re
 
 const category_url = "https://tasty-treats-backend.p.goit.global/api/categories";
 
+//const fetchOategiries = async () => {
+ // const response = await axios.get(URL);
+//  const results = await response;
+ // return results;
+//};
 axios.get(category_url).then(response => {
   const recipes_markup = response.data.map((category) =>
 
     `<a class="nav-scroller__item" href="#">${category.name}</a>`
   ).join("");
   recipes.innerHTML = recipes_markup;
-  
-  
+
+
 
   recipes.addEventListener("click", (evt) => {
  
@@ -271,10 +290,11 @@ axios.get(category_url).then(response => {
 
            //    gallery.innerHTML=`<div class="plug"><svg class="icon-plug"><use href="./images/sprite/icons.svg#icon-elements"></use></svg>
           //    <p class="plug-text">Sorry, there are no images matching your search query. Please try again</p></div>`;
+          Notiflix.Report.failure('Error', 'Sorry, there are no images matching your search query. Please try again', function retry() {
+            return;
+          },); 
 
-
-               return;
-               }else{
+            }else{
                 pictures.push(response.data);
 
           renderCardsList(response.data);
@@ -340,7 +360,7 @@ allCategories.addEventListener("click",()=>{
 
 
 
-function initRatings(){
+/*function initRatings(){
   let ratingActive, ratingValue;
   for (let index=0; index<ratings.length; index++){
       const rating=ratings[index];
@@ -362,5 +382,5 @@ function initRatings(){
       const ratingActiveWidth = index/0.05;
       ratingActive.style.width=`${ratingActiveWidth}%`;
   }
-}
+}*/
 
